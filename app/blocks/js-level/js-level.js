f08546e41d5fa57ee48c204e2d439b00a5f2bf7a
Artyom Resh <artyomresh@gmail.com>
2016-10-25 14:03:59 +0400

    Улучшил скрипт js-level. Подкачал кроссплатформенность и кроссбраузерность анкеты

--- app/blocks/js-level/js-level.js
+++ app/blocks/js-level/js-level.js
@@ -1,6 +1,63 @@
-document.querySelector('.js-level__scale').onclick = function (e) {
-	const x = e.pageX;
+var sliderElem = document.querySelector('.js-level__scale');
+var thumbElem = document.querySelector('.js-level__pointer');
 
-	const pointer = document.querySelector('.js-level__pointer');
-	pointer.style.marginLeft = x - 135 + 'px';
+thumbElem.onmousedown = function(e) {
+  var thumbCoords = getCoords(thumbElem);
+  var shiftX = e.pageX - thumbCoords.left;
+  // shiftY здесь не нужен, слайдер двигается только по горизонтали
+
+  var sliderCoords = getCoords(sliderElem);
+	//  вычесть координату родителя, т.к. position: relative
+
+  document.onmousemove = function(e) {
+		var newLeft = e.pageX - shiftX - sliderCoords.left;
+    // курсор ушёл вне слайдера
+    if (newLeft < 0) {
+      newLeft = 0;
+    }
+    var rightEdge = sliderElem.offsetWidth - thumbElem.offsetWidth;
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
+    }
+
+    thumbElem.style.left = newLeft + 'px';
+  }
+
+  document.onmouseup = function() {
+    document.onmousemove = document.onmouseup = null;
+  };
+
+  return false; // disable selection start (cursor change)
+};
+
+
+sliderElem.onclick = function(e) {
+	// Что бы работало на touch устройствах
+  var sliderCoords = getCoords(sliderElem);
+
+	sliderElem.onclick = function (e) {
+		var newLeft = e.pageX - 10 - sliderCoords.left;
+		if (newLeft < 0) {
+      newLeft = 0;
+    }
+    var rightEdge = sliderElem.offsetWidth - thumbElem.offsetWidth;
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
+    }
+		
+		thumbElem.style.left = newLeft + 'px';
+	}
+};
+
+thumbElem.ondragstart = function() {
+  return false;
+};
+
+function getCoords(elem) { // кроме IE8-
+  var box = elem.getBoundingClientRect();
+
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  };
 };
